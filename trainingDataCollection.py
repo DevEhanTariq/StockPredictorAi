c9f3716053c97615865126013d895975bf55dc3e
DevET <ehan.tariq.21@icloud.com>
2026-06-22 22:04:31 +0100

    Feat: Now fully handles all USA stocks that are also in Yahoo Finance
    - Now fully handles all USA stocks that are also in Yahoo Finance.

--- trainingDataCollection.py
+++ trainingDataCollection.py
@@ -1,3 +1,7 @@
+from os import write
+
+from pygments.lexers import json5
+
 from libraries import *
 
 def trainingSettings():
@@ -11,6 +15,7 @@ class trainingDataCollection:
     def __init__(self):
         self.tickers: list[str] = []
         self.validTickers: list[str] = []
+        self.data: dict[str:] = {}
 
     def compileTickers(self): # Creates a list of all Stocks in nasdaqtrader
         url = "https://www.nasdaqtrader.com/dynamic/SymDir/nasdaqlisted.txt"
@@ -23,20 +28,47 @@ class trainingDataCollection:
 
         self.tickers = list(set(tickers + nyse["ACT Symbol"].dropna().tolist())) # Puts the list of Stocks in self.tickers variable
 
-    def checkValidTickers(self, showStocks: bool=False): # Checks if all tickers are in Yahoo Finance
+    def checkValidTickers(self, showStocks: bool=False, progressCheck: bool=False): # Checks if all tickers are in Yahoo Finance
         ts = trainingSettings()
         searchLength = ts[1]
         for t in self.tickers[:searchLength]:  # Loops through all tickers and checks if they are in Yahoo Finance
             if  showStocks:
                 print(t)
             try:
-                if not yf.Tickers(t).history(period="5d", progress=False).empty:
+                if not yf.Tickers(t).history(period="5d", progress=progressCheck).empty:
                     self.validTickers.append(t)
             except:
                 pass
+        if showStocks:
+            print(self.validTickers)
+
+    def compileTrainingData(self, progressCheck: bool=False): # Saves all data in a dictionary
+        ts = trainingSettings()
+        periodTime: str = ts[0]
+
+        validTickerString: str = ""
+        for i in range(len(self.validTickers)): # Creates a string containing all valid tickers
+            if self.validTickers[i] == self.validTickers[-1]:
+                validTickerString += self.validTickers[i]
+            else:
+                validTickerString += self.validTickers[i] + " "
+        tickers = yf.Tickers(validTickerString)
+
+        hist = tickers.history(period=periodTime, progress=progressCheck)
+        for j in range(len(self.validTickers)):  # Loops through all Stocks in Yahoo Finance
+            histNew = hist[j]
+            rows = hist.values.tolist()
+            self.data[self.validTickers[j]] = rows
+
+    def saveTrainingData(self): # Saves self.data to json5
+        data = self.data
+        with open("./TrainingData/stockData.json5", "w") as f: # Opens stockData.json5
+            json5Data = json5.loads(data)
+            json5.dump(json5Data, f)
 
 if __name__ == "__main__":
-    tdc = trainingDataCollection()
+    tdc = trainingDataCollection() # Redefines the class as tdc
     tdc.compileTickers()
     tdc.checkValidTickers(showStocks=True)
-    print(tdc.validTickers)
+    #tdc.compileTrainingData()
+    #tdc.saveTrainingData()
